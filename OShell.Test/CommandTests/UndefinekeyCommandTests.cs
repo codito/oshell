a39a4905f44fb34abf9c57679b1b90f74ee6faf4
Arun Mahapatra <pratikarun@gmail.com>
2013-06-21 20:55:40 +0530

    Add undefinekey command.

---
+++ OShell.Test/CommandTests/UndefinekeyCommandTests.cs
@@ -0,0 +1,85 @@
+﻿namespace OShell.Test.CommandTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NSubstitute;
+
+    using OShell.Core.Commands;
+    using OShell.Core.Contracts;
+    using OShell.Core.Services;
+
+    [TestClass]
+    public class UndefinekeyCommandTests
+    {
+        private const string TestKeyMap = "TestKeyMap";
+
+        private readonly IKeyMapService keyMapService;
+
+        public UndefinekeyCommandTests()
+        {
+            var platformFacade = Substitute.For<IPlatformFacade>();
+            this.keyMapService = new KeyMapService(platformFacade);
+            this.keyMapService.AddKeyMap(TestKeyMap);
+        }
+
+        [TestMethod]
+        public void UndefinekeyCommandHasNameAsUndefinekey()
+        {
+            var undefinekey = new UndefinekeyCommand();
+            undefinekey.Name.Should().Be("undefinekey");
+        }
+
+        [TestMethod]
+        public void UndefinekeyCommandHanlderThrowsForNullKeyMapService()
+        {
+            var action = new Action(() => new UndefinekeyCommandHandler(null));
+            action.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("keyMapService");
+        }
+
+        [TestMethod]
+        public void UndefinekeyCommandHandlerExecuteThrowsForNullCommand()
+        {
+            var action = new Action(() => new UndefinekeyCommandHandler(this.keyMapService).Execute(null));
+            action.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("command");
+        }
+
+        [TestMethod]
+        public void UndefinekeyCommandHandlerExecuteThrowsForInvalidCommandArgs()
+        {
+            var invalidArgs = new[] { null, string.Empty, "arg1", "arg1 arg2 arg3", TestKeyMap + " NotAKey" };
+            var undefinekeyCommandHandler = new UndefinekeyCommandHandler(this.keyMapService);
+            foreach (var invalidArg in invalidArgs)
+            {
+                var undefinekeyCommand = new UndefinekeyCommand { Args = invalidArg };
+                Func<Task> action = async () => await undefinekeyCommandHandler.Execute(undefinekeyCommand);
+                action.ShouldThrow<ArgumentException>();
+            }
+        }
+
+        [TestMethod]
+        public async Task UndefinekeyCommandHandlerExecuteUnregistersAKeySequence()
+        {
+            var keySequences = new[] { "A", "Control+T", "Control+Shift+Alt+T" };
+            var undefinekeyCommandHandler = new UndefinekeyCommandHandler(this.keyMapService);
+            var keymap = this.keyMapService.GetKeyMapByName(TestKeyMap);
+            foreach (var keySequence in keySequences)
+            {
+                var undefinekeyCommand = new UndefinekeyCommand { Args = TestKeyMap + " " + keySequence };
+                (await undefinekeyCommandHandler.Execute(undefinekeyCommand)).Should().BeTrue();
+
+                var registerKey = new Action(
+                    () =>
+                        {
+                            keymap.RegisterAction((Keys)new KeysConverter().ConvertFrom(keySequence), args => Task.Run(() => true));
+                        });
+                registerKey.ShouldNotThrow();
+            }
+        }
+    }
+}
---
+++ OShell/Core/Commands/UndefinekeyCommand.cs
@@ -0,0 +1,97 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UndefinekeyCommand.cs" company="OShell Development Team">
+//     Copyright (c) OShell Development Team. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the UndefinekeyCommand type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OShell.Core.Commands
+{
+    using System;
+    using System.Globalization;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    using OShell.Core.Contracts;
+
+    /// <summary>
+    /// The <code>undefinekey</code> command.
+    /// </summary>
+    public class UndefinekeyCommand : ICommand
+    {
+        /// <inheritdoc/>
+        public string Name
+        {
+            get
+            {
+                return "undefinekey";
+            }
+        }
+
+        /// <inheritdoc/>
+        public string Args { get; set; }
+
+        /// <inheritdoc/>
+        public string Help
+        {
+            get
+            {
+                return "undefinekey keymap key\nRemove the binding for key from keymap.";
+            }
+        }
+    }
+
+    /// <summary>
+    /// The <code>undefinekey</code> command handler.
+    /// </summary>
+    public class UndefinekeyCommandHandler : ICommandHandler<UndefinekeyCommand>
+    {
+        private readonly IKeyMapService keyMapService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UndefinekeyCommandHandler"/> class.
+        /// </summary>
+        /// <param name="keyMapService">The <code>keymap</code> service.</param>
+        public UndefinekeyCommandHandler(IKeyMapService keyMapService)
+        {
+            if (keyMapService == null)
+            {
+                throw new ArgumentNullException("keyMapService");
+            }
+
+            this.keyMapService = keyMapService;
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> Execute(UndefinekeyCommand command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException("command");
+            }
+
+            var commandArgs = string.IsNullOrEmpty(command.Args) ? null : command.Args.Split(' ');
+            if (commandArgs == null || commandArgs.Length != 2)
+            {
+                throw new ArgumentException("Incorrect definekey command arguments: " + command.Args);
+            }
+
+            return Task.Run(
+                () =>
+                    {
+                        var keymap = this.keyMapService.GetKeyMapByName(commandArgs[0]);
+                        var keySequence = new KeysConverter().ConvertFrom(null, CultureInfo.CurrentCulture, commandArgs[1]);
+                        if (keySequence == null)
+                        {
+                            throw new ArgumentException(
+                                "Invalid key sequence provided to undefinekey command. Keys: " + commandArgs[1]);
+                        }
+
+                        keymap.UnregisterAction((Keys)keySequence);
+                        return true;
+                    });
+        }
+    }
+}
